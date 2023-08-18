fdc3e54202eb9a58038df896d757bdd3363a5bff
hector <hectorrrivera1010@gmail.com>
2023-08-17 21:51:34 -0600

    initial commit

---
+++ app/components/ToggleMode.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import {Tabs, Tab} from "@nextui-org/react";
+import {BsSun, BsMoon} from 'react-icons/bs';
+import { useEffect, useState } from "react";
+
+const ToggleMode = () => {
+
+  const [theme, setTheme] = useState('light')
+
+
+  useEffect(()=> {
+    document.documentElement.className = theme
+  },[theme])
+
+  const handleThemeChange = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <>
+        <Tabs 
+        selectedKey={theme}
+        onClick={handleThemeChange}>
+            <Tab 
+            key="light"
+            title={
+                <BsSun/>
+            }>
+            </Tab>
+            <Tab
+            key="dark"
+            title={
+                <BsMoon/>
+            }>
+            </Tab>
+        </Tabs>
+    </>
+  )
+}
+
+export default ToggleMode
